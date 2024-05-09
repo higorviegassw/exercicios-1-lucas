14a9b6ac9c3c8e75773d64977663dd1bf5a2a44d
Higor Viegas <viegas.higor@gmail.com>
2024-05-09 12:11:07 -0300

    Correção de alguns erros de teste

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,11 +2,9 @@
 
 namespace Chuva\Php\WebScrapping;
 
-//  ADICIONA AS BIBLIOTECAS AO CÓDIGO
+//ADICIONA AS BIBLIOTECAS AO CÓDIGO.
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
-use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
-use Box\Spout\Common\Entity\Style\CellAlignment;
 
 /**
  * Runner for the Webscrapping exercice.
@@ -24,22 +22,14 @@ class Main {
 
     // Write your logic to save the output file bellow.
     // print_r($data);
+    // DEFINE DIRETÓRIO E NOME DO ARQUIVO A SER CRIADO.
+    $filePath = __DIR__ . '/model.xlsx';
 
-    // DEFINE DIRETÓRIO E NOME DO ARQUIVO A SER CRIADO
-    $filePath = __DIR__ . '/../../assets/model.xlsx';
-    $tempFilePath = __DIR__ . '/../../assets/model copy.xlsx';
-
-    // $writer = WriterEntityFactory::createWriterFromFile(__DIR__ . '/../../assets/test.xlsx');
-    // $reader = ReaderEntityFactory::createReaderFromFile(__DIR__ . '/../../assets/test.xlsx');
-    // $reader = ReaderEntityFactory::createXLSXReader();
-    // $reader->open($filePath);
-
-    // CRIA UM OBJETO PARA ESCREVER PLANILHA XLSX E ABRE O ARQUIVO
+    // CRIA UM OBJETO PARA ESCREVER PLANILHA XLSX E ABRE O ARQUIVO.
     $writer = WriterEntityFactory::createXLSXWriter();
-    // $writer->openToFile($tempFilePath);
     $writer->openToFile($filePath);
 
-    // DEFINE ESTILOS DA PRIMEIRA LINHA E DOS DADOS
+    // DEFINE ESTILOS DA PRIMEIRA LINHA E DOS DADOS.
     $firstRowStyle = (new StyleBuilder())
            ->setFontName('Arial')
            ->setFontSize(11)
@@ -50,116 +40,44 @@ class Main {
            ->setFontName('Arial')
            ->setFontSize(11)
            ->build();
-    
 
-    //print_r($writer);
-    // $counter = 0;
-    // foreach ($reader->getSheetIterator() as $sheet) {
-    //   foreach ($sheet->getRowIterator() as $row) {
-    //     //  IGNORA A PRIMEIRA LINHA
-    //       //if ($counter++ == 0) continue;
-
-          
-    //       //  DUAS FORMAS DE FAZER: TRANSFORMAR A $row EM ARRAY
-    //       $cells = $row->toArray();
-    //       //  RETIRAR AS CELULAS DA LINHA
-    //       //print_r($row->getCells());
-    //       //print_r($cells);
-
-    //       //  FILTRO PARA RETIRAR ENTRADAS VAZIAS
-    //       print_r(array_filter($cells, fn($value) => !is_null($value) && $value !== ''));
-          
-    //       foreach($row->getCells() as $cell) {
-    //         print_r($cell->getValue()."\n");
-    //         //if($cell != "") print_r("\n".$cell);
-    //       }
-          
-    //   }
-    // }
-
-    
-    // ID	Title	Type	Author 1	Author 1 Institution	Author 2	Author 2 Institution	Author 3	Author 3 Institution	Author 4	Author 4 Institution	Author 5	Author 5 Institution	Author 6	Author 6 Institution	Author 7	Author 7 Institution	Author 8	Author 8 Institution	Author 9	Author 9 Institution
 
     // DEFINE TEXTO CABEÇALHO (1 LINHA)
     $firstRow = array("ID", "Title", "Type");
-    // COMO A MAIOR QUANTIDADE DE AUTORES É 16, ESCREVE 16 VEZES
+    // COMO A MAIOR QUANTIDADE DE AUTORES É 16, ESCREVE 16 VEZES.
     for($x = 1; $x <= 16; $x++) {
       // print("Author ".$x);
       array_push($firstRow, "Author ".$x, "Author ".$x." Institution");
     }
-    // ADICIONA PRIMEIRA LINHA A PLANILHA
+    // ADICIONA PRIMEIRA LINHA A PLANILHA.
     $writer->addRow(
       WriterEntityFactory::createRowFromArray($firstRow, $firstRowStyle)
     );
 
     // PARA CADA DADO RETORNADO DA FUNÇÃO 'scrap()'
     foreach($data[0] as $paper) {
-      // PREPARA UM ARRAY PARA SER INSERIDO NAS LINHAS SEGUINTES
-      //print_r("\n");
+      // PREPARA UM ARRAY PARA SER INSERIDO NAS LINHAS SEGUINTES.
       $newRow = array(
         $paper->id,
         $paper->title,
         $paper->type
       );
-      // PARA CADA AUTOR DO PAPER ADICIONA SEU NOME E INSTITUIÇÃO
+      // PARA CADA AUTOR DO PAPER ADICIONA SEU NOME E INSTITUIÇÃO.
       foreach($paper->authors as $author) {
         array_push($newRow, $author->name);
         array_push($newRow, $author->institution);
       }
 
 
-      // ADICIONA A LINHA NA PLANILHA
-      //print_r($newRow);
+      // ADICIONA A LINHA NA PLANILHA.
       $writer->addRow(
         WriterEntityFactory::createRowFromArray($newRow, $style)
       );  
     }
 
-    
-    
-    // print_r("\n");
-    // print_r($newRow);
-
-
-    // At this point, the new spreadsheet contains the same data as the existing one.
-    // So let's add the new data:
-    // $writer->addRow(
-    //   WriterEntityFactory::createRowFromArray(['2015-12-25', 'Christmas gift', 29, 'USD'], $style)
-    // );
-    
-    //$reader->close();
-    // ENCERRA CONEXÃO COM O ARQUIVO
+    // ENCERRA CONEXÃO COM O ARQUIVO.
     $writer->close();
 
-    
-    
-
-    // unlink($filePath);
-    // copy($tempFilePath, $filePath);
-    // unlink($tempFilePath);
   }
 
 }
-/**
- * LINHA 1
- * ID
- * Title
- * Type
- * Author X
- * Author X Institution
- * X 1~9
- * 
- * LINHA 2
- * [DADOS]
- */
-
- /*
-  LINKS
-  https://products.fileformat.com/pt/spreadsheet/php/spout/ 
-  https://stackoverflow.com/questions/3654295/remove-empty-array-elements
-  https://opensource.box.com/spout/docs/
-  
-  
-  */
-
-  // TESTE
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -15,40 +15,39 @@ class Scrapper {
    */
   public function scrap(\DOMDocument $dom): array {
 
-
     $xpath = new \DomXPath($dom);
 
-    //  RETORNA TODOS AS TAGS "a" COM CLASSE 'paper-card'
+    // RETORNA TODOS AS TAGS "a" COM CLASSE 'paper-card'
     $paperCards = $xpath->query("//a[contains(@class,'paper-card')]");
 
-    $objPapersArray = array();
+    $objPapersArray = [];
 
-    //  PARA CADA 'a.paper-card'
+    // PARA CADA 'a.paper-card'
     foreach($paperCards as $paper) {
       
-      //  RETORNA CADA CAMPO ESPECÍFICO DE CADA PAPER
+      // RETORNA CADA CAMPO ESPECÍFICO DE CADA PAPER.
       $id = $xpath->query(".//following::div[contains(@class,'volume-info')]", $paper);
       $title = $xpath->query(".//h4[contains(@class,'paper-title')]", $paper);
       $type = $xpath->query(".//following::div[contains(@class,'tags')]", $paper);
 
 
-      //  RETORNA A DIV DA LISTA DE AUTORES 'div.authors' DO PAPER
+      // RETORNA A DIV DA LISTA DE AUTORES 'div.authors' DO PAPER.
       $authorsDiv = $xpath->query(".//following::div[contains(@class,'authors')]", $paper);
 
       // PARA CADA 'span' EM 'div.authors'
       $authors = $xpath->query(".//span", $authorsDiv[0]);
 
-      //  CRIA ARRAY DE OBJETOS DE AUTORES
+      // CRIA ARRAY DE OBJETOS DE AUTORES.
       $objAuthorsArray = array();
       foreach($authors as $author) {
-        //  CRIA ARRAY INDIVIDUAL PARA CADA AUTOR
-        //  CONDICIONAL NECESSÁRIO POIS NO ID 137475 APÓS RAFAEL ALVES DE ANDRADE EXISTE UM CAMPO VAZIO
+        // CRIA ARRAY INDIVIDUAL PARA CADA AUTOR.
+        // CONDICIONAL NECESSÁRIO POIS NO ID 137475 APÓS RAFAEL ALVES DE ANDRADE EXISTE UM CAMPO VAZIO.
         if($author->nodeValue != "") {
-          //  RETORNA O VALOR ABSOLUTO DO NÓ
+          // RETORNA O VALOR ABSOLUTO DO NÓ.
           $authorName = str_replace(";", "", $author->nodeValue);
-          //  RETORNA O VALOR DO ATRIBUTO 'title'
+          // RETORNA O VALOR DO ATRIBUTO 'title'
           $authorInstitution = $author->attributes["title"]->nodeValue;
-          //  INSERE NO ARRAY INDIVIDUAL NOME E INSTITUIÇÃO DO AUTOR
+          // INSERE NO ARRAY INDIVIDUAL NOME E INSTITUIÇÃO DO AUTOR.
           array_push($objAuthorsArray, new Person($authorName, $authorInstitution));
         }
       }
@@ -63,38 +62,9 @@ class Scrapper {
       array_push($objPapersArray, $objPaper);
     }
 
-
-
     return [
       $objPapersArray,
     ];
   }
 
 }
-
-
-/* ESTRUTURA DO ARRAY
-[
-    [
-        "id" => "123",
-        "title" => "TITULO",
-        "type" => "TIPO",
-        "authors" => ["name" => "NOME DO AUTOR", "institution" => "INSTITUIÇÃO"]
-    ]
-]
-*/
-
-/*
-PHP links
-https://scrapfly.io/blog/how-to-select-elements-by-class-in-xpath/
-https://www.php.net/manual/en/class.domxpath.php
-https://www.php.net/manual/en/domxpath.construct.php
-https://stackoverflow.com/questions/48085772/php-domxpath-loop-through-search-and-find-child-div-value
-https://www.php.net/manual/en/function.count.php
-https://www.php.net/manual/pt_BR/control-structures.for.php
-https://www.php.net/manual/en/function.str-replace.php
-https://www.php.net/manual/en/function.array-keys.php
-https://www.php.net/manual/en/function.array-push.php
-https://stackoverflow.com/questions/676677/how-to-add-elements-to-an-empty-array-in-php
-https://stackoverflow.com/questions/1544176/accessing-arrays-inside-arrays-in-php
-*/
